3210ec7f2f96c4ec3b0881cb1cfa5888c30cf37b
Justine MANGA <jmarymanga@gmail.com>
2021-10-01 16:20:01 +0000

    P13_JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import img2InSrc from './img2InSrc.jpg';
 import './App.css';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black;max-width:100vw"}} className="container">
+
+      <h1 className="title red">Justine Marylène MANGA</h1>
+
+      <br /> 
+
+      <img src={img2InSrc} alt="ImgSrc" />
+
+      <br /><br />  
+
+      <img src="/imgInPublic.jpg" alt="Img In Public" />
+
+      <br /><br />
+
+      <iframe width="320" height="240" src="https://www.youtube.com/embed/LwU798HJIrY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
+     
     </div>
+
+    
   );
 }
 
